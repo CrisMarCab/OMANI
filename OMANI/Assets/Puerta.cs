858e7506471bd4a50a2c1429916391799e7c5141
CrisMarCab <crisw8mc@gmail.com>
2018-01-15 18:17:17 +0100

    pruebas??? Creo que he roto algo

--- OMANI/Assets/Puerta.cs
+++ OMANI/Assets/Puerta.cs
@@ -2,39 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Puerta : MonoBehaviour
-{
+public class Puerta : MonoBehaviour {
 
     Vector3 startPosition;
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void AbrirPuerta()
-    {
+    public void AbrirPuerta() {
         this.GetComponent<Animation>().Play();
         this.GetComponent<AudioSource>().Play();
     }
 
     private void LateUpdate()
     {
-
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            AbrirPuerta();
-        }
     }
 }
-
